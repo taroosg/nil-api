f6ca928cb0877975197209df8b3e25d20296bf4a
taroosg <macchinetta.xyz@gmail.com>
2020-01-27 22:31:18 +0900

    add client

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
+
+
 app.get('/api/v1/request', (req, res) => {
   console.log(req.query)
   tweetPost(req.query.tweet);
